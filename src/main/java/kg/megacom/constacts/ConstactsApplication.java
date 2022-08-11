d783489ed23b3fb1e5dbf8d7d483d99ce7b399cb
TelidaTG <tamga.tg@gmail.com>
2022-08-11 13:44:19 +0600

    iylgiy

---
+++ src/main/java/kg/megacom/constacts/ConstactsApplication.java
@@ -0,0 +1,13 @@
+package kg.megacom.constacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConstactsApplication.class, args);
+    }
+
+}
